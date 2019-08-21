0e006936a69ccb114fa582b897f870ec14f0aeb1
Mirian Okradze <okradzemirian@gmail.com>
2019-08-21 20:53:53 +0400

    Migrating to mongoose: Added hotel model

--- packages/server/src/models/Hotel.js
+++ packages/server/src/models/Hotel.js
@@ -1,54 +1,34 @@
 import mongoose from 'mongoose'
-import { createHotelSchema } from '@hb/common'
-import validateField from '../utils/validateField'
 
-const Hotel = mongoose.Schema(
+const hotelSchema = mongoose.Schema(
     {
         id: {
+            id: mongoose.ObjectId,
             required: true,
         },
         email: {
             type: String,
             required: true,
-            validate: {
-                validator(value) {
-                    validateField(createHotelSchema, 'email', value)
-                },
-            },
             unique: true,
         },
         phone: {
             type: String,
             required: true,
-            validate: {
-                validator(value) {
-                    validateField(createHotelSchema, 'phone', value)
-                },
-            },
             unique: true,
         },
         name: {
             type: String,
             required: true,
-            validate: {
-                validator(value) {
-                    validateField(createHotelSchema, 'name', value)
-                },
-            },
         },
         password: {
             type: String,
             required: true,
-            validate: {
-                validator(value) {
-                    validateField(createHotelSchema, 'password', value)
-                },
-            },
         },
     },
     {
         timestamps: { createdAt: true, updatedAt: false },
     },
 )
+const Hotel = mongoose.model('Hotel', hotelSchema)
 
 export default Hotel
