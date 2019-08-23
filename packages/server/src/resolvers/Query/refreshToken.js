9e638008eaa0abc3ae790939dc9958b4936b9b0a
Mirian Okradze <okradzemirian@gmail.com>
2019-08-23 11:56:54 +0400

    Migrating to mongoose: Modified refreshToken

--- packages/server/src/resolvers/Query/refreshToken.js
+++ packages/server/src/resolvers/Query/refreshToken.js
@@ -1,23 +1,15 @@
+import Hotel from '../../models/Hotel'
 import getHotelId from '../../utils/getHotelId'
 import generateToken from '../../utils/generateToken'
 import setCookie from '../../utils/setCookie'
 
-export default async function refreshToken(
-    parent,
-    args,
-    { req, res, prisma },
-    info,
-) {
+export default async function refreshToken(parent, args, { req, res }) {
     const hotelId = getHotelId({ req })
 
-    const hotel = await prisma.query.hotel(
-        {
-            where: { id: hotelId },
-        },
-        info,
-    )
+    const hotel = await Hotel.findById(hotelId)
 
     const token = generateToken(hotelId)
+
     setCookie({ res }, token)
 
     return hotel
